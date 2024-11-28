6b9c7aec57ab67a4392db5ed212ee8b93eaa0371
suitougreentea <suitougreentea@users.noreply.github.com>
2024-11-28 21:46:04 +0900

    fix play screen binding in single match mode

--- client/play_screen.ts
+++ client/play_screen.ts
@@ -17,63 +17,87 @@ const client = await denocg.getClient<TypeDefinition>({
   socketPort: 8515,
 });
 
-const playerNames = document.querySelectorAll<HTMLDivElement>(
-  ".player .names .name",
-);
-const playerHearts = document.querySelectorAll<HTMLDivElement>(
-  ".player .names .hearts",
-);
-const playerProfiles = document.querySelectorAll<WakutetProfileCardElement>(
-  ".player .profile wakutet-profile-card",
-);
-const playerProfileContainers = document.querySelectorAll<HTMLDivElement>(
-  ".player .profile",
-);
-
-const getCombinedIndex = (matchIndex: number, playerIndex: number) => {
-  return matchIndex * 2 + playerIndex;
+const nodeListToArray = <T extends Node>(nodeList: NodeListOf<T>) => {
+  const result = new Array<T>(nodeList.length);
+  nodeList.forEach((e, i) => result[i] = e);
+  return result;
 };
+
+const matchElements = [...new Array(numMatches)].map((_, matchIndex) => {
+  const matchNumber = matchIndex + 1;
+  const matchElement = document.querySelector(`#match${matchNumber}`);
+  if (matchElement != null) {
+    return {
+      playerNames: nodeListToArray(matchElement.querySelectorAll(".player .names .name")) as HTMLDivElement[],
+      playerHearts: nodeListToArray(matchElement.querySelectorAll(".player .names .hearts")) as HTMLDivElement[],
+      playerProfiles: nodeListToArray(matchElement.querySelectorAll(".player .profile wakutet-profile-card")) as WakutetProfileCardElement[],
+      playerProfileContainers: nodeListToArray(matchElement.querySelectorAll(".player .profile")) as HTMLDivElement[],
+    };
+  } else {
+    return {
+      playerNames: [undefined, undefined],
+      playerHearts: [undefined, undefined],
+      playerProfiles: [undefined, undefined],
+      playerProfileContainers: [undefined, undefined],
+    }
+  }
+});
 
 const getPlayerNameElement = (matchIndex: number, playerIndex: number) => {
-  return playerNames[getCombinedIndex(matchIndex, playerIndex)];
+  return matchElements[matchIndex].playerNames[playerIndex];
 };
 const getPlayerHeartsElement = (matchIndex: number, playerIndex: number) => {
-  return playerHearts[getCombinedIndex(matchIndex, playerIndex)];
+  return matchElements[matchIndex].playerHearts[playerIndex];
 };
 const getPlayerProfileElement = (matchIndex: number, playerIndex: number) => {
-  return playerProfiles[getCombinedIndex(matchIndex, playerIndex)];
+  return matchElements[matchIndex].playerProfiles[playerIndex];
 };
 const getPlayerProfileContainerElement = (matchIndex: number, playerIndex: number) => {
-  return playerProfileContainers[getCombinedIndex(matchIndex, playerIndex)];
+  return matchElements[matchIndex].playerProfileContainers[playerIndex];
 };
 
 const setPlayerName = (matchIndex: number, playerIndex: number, name: NameData) => {
-  const element = getPlayerNameElement(matchIndex, playerIndex);
-  const originalName = name.original != "" ? name.original : `Player${playerIndex + 1}`;
-  element.innerText = toFullwidthName(originalName);
-  if (name.english) {
-    const english = document.createElement("span");
-    english.className = "english";
-    english.innerText = `(${name.english})`;
-    element.appendChild(english);
+  const playerNameElement = getPlayerNameElement(matchIndex, playerIndex);
+  if (playerNameElement != null) {
+    const originalName = name.original != "" ? name.original : `Player${playerIndex + 1}`;
+    playerNameElement.innerText = toFullwidthName(originalName);
+    if (name.english) {
+      const english = document.createElement("span");
+      english.className = "english";
+      english.innerText = `(${name.english})`;
+      playerNameElement.appendChild(english);
+    }
   }
-  getPlayerProfileElement(matchIndex, playerIndex).name = name;
+
+  const playerProfileElement = getPlayerProfileElement(matchIndex, playerIndex);
+  if (playerProfileElement != null) {
+    playerProfileElement.name = name;
+  }
 };
 
 const setPlayerHearts = (matchIndex: number, playerIndex: number, hearts: HeartsData) => {
   const element = getPlayerHeartsElement(matchIndex, playerIndex);
-  element.innerHTML = [...new Array(hearts.max)].map((_, i) =>
-    `<div class="${i < hearts.lit ? "lit" : "unlit"}">Œ</div>`
-  ).join("");
+  if (element != null)
+  {
+    element.innerHTML = [...new Array(hearts.max)].map((_, i) =>
+      `<div class="${i < hearts.lit ? "lit" : "unlit"}">Œ</div>`
+    ).join("");
+  }
 };
 
 const setPlayerProfile = (matchIndex: number, playerIndex: number, profile: ProfileData) => {
-  getPlayerProfileElement(matchIndex, playerIndex).profile = profile;
+  const element = getPlayerProfileElement(matchIndex, playerIndex);
+  if (element != null) {
+    element.profile = profile;
+  }
 }
 
 const setPlayerProfileVisible = (matchIndex: number, visible: boolean) => {
   [0, 1].forEach(playerIndex => {
-    getPlayerProfileContainerElement(matchIndex, playerIndex).hidden = !visible;
+    const container = getPlayerProfileContainerElement(matchIndex, playerIndex);
+    if (container != null) {
+      container.hidden = !visible;
+    }
   });
 }
 
